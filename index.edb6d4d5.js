c83498eadff62ea03f59a679d7ba36b93ac40f1a
Anastasiia <vasilisandreeva00@gmail.com>
2024-07-01 15:52:04 +0300

    make build

---
+++ index.edb6d4d5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=p(getComputedStyle(e).width),o=p(getComputedStyle(e).height),u=p(getComputedStyle(t).width),d=p(getComputedStyle(t).height);function p(e){return parseFloat(e)}t.style.left=l/2-u/2+"px",t.style.top=o/2-d/2+"px";
+//# sourceMappingURL=index.edb6d4d5.js.map
